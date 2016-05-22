103a0e0e4c9e527445b9935eea78bcbda39c0b67
Rohit Shankarrao Ukirde <ukirderohit@gmail.com>
2016-05-21 19:49:01 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Rohit on 5/21/2016.
+ */
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("Hello, World !!!");
+
+        int myFirstNumber = 5 + 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber ;
+        System.out.println("Hello Rohit");
+        System.out.println(myTotal);
+
+    }
+}
